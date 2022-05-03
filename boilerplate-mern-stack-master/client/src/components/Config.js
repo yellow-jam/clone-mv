d65316b101fa2ae07641433e618c4fb79e54ae46
unknown <lerle144@gmail.com>
2022-05-03 18:23:29 +0900

    ~3강 몽고DB 설정, URL 상수 설정

--- boilerplate-mern-stack-master/client/src/components/Config.js
+++ boilerplate-mern-stack-master/client/src/components/Config.js
@@ -3,4 +3,4 @@ export const USER_SERVER = '/api/users';
 
 
 export const API_URL = 'https://api.themoviedb.org/3/';
-IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/';
+export const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/';
