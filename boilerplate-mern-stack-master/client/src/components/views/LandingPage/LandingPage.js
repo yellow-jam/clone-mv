f24241b4506faffb05ba37fa589dbcadf7ed79c2
unknown <lerle144@gmail.com>
2022-05-03 21:14:43 +0900

    6강 Load More 버튼 만들기

--- boilerplate-mern-stack-master/client/src/components/views/LandingPage/LandingPage.js
+++ boilerplate-mern-stack-master/client/src/components/views/LandingPage/LandingPage.js
@@ -9,22 +9,36 @@ function LandingPage() {
 
     const [Movies, setMovies] = useState([])
     const [MainMovieImage, setMainMovieImage] = useState(null)
+    const [CurrentPage, setCurrentPage] = useState(0)
 
     useEffect(() => {
         const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
-        
+        fetchMovies(endpoint)
+
+    }, [])
+
+    const fetchMovies = endpoint => {
+
         fetch(endpoint)
         .then(response => response.json())
         .then(response => {
 
-            console.log(response.results)
+            console.log(response)
             
-            setMovies([...response.results])
+            setMovies([...Movies, ...response.results])
             setMainMovieImage(response.results[0])
+            setCurrentPage(response.page)
 
         })
 
-    }, [])
+    }
+
+    const loadMoreItems = () => {
+
+        const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=${CurrentPage + 1}`;
+        fetchMovies(endpoint)
+
+    }
 
     return (
         <div style={{ width: '100%', margin: '0' }}>
@@ -64,7 +78,7 @@ function LandingPage() {
             </div>
             
             <div style={{ display: 'flex', justifyContent: 'center'}}>
-                <button>Load More</button>
+                <button onClick={loadMoreItems}>Load More</button>
             </div>
         </div>
     
