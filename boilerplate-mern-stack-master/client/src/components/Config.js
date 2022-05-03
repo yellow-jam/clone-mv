ce89b944b1c02ddd756139a30425532ddc758d8a
unknown <lerle144@gmail.com>
2022-05-03 20:34:53 +0900

    4강 Landing Page 만들기 (1)

--- boilerplate-mern-stack-master/client/src/components/Config.js
+++ boilerplate-mern-stack-master/client/src/components/Config.js
@@ -3,4 +3,5 @@ export const USER_SERVER = '/api/users';
 
 
 export const API_URL = 'https://api.themoviedb.org/3/';
+export const API_KEY = '7ade7b8d02768a48316ff58c47a7ab14';
 export const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/';
--- boilerplate-mern-stack-master/client/src/components/views/LandingPage/LandingPage.js
+++ boilerplate-mern-stack-master/client/src/components/views/LandingPage/LandingPage.js
@@ -1,11 +1,54 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { FaCode } from "react-icons/fa";
+import { API_URL, API_KEY, IMAGE_BASE_URL } from '../../Config';
+import MainImage from './Sections/MainImage';
 
 function LandingPage() {
-    return (
-        <>
+
+    const [Movies, setMovies] = useState([])
+    const [MainMovieImage, setMainMovieImage] = useState(null)
+
+    useEffect(() => {
+        const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
         
-        </>
+        fetch(endpoint)
+        .then(response => response.json())
+        .then(response => {
+
+            console.log(response)
+            
+            setMovies([...response.results])
+            setMainMovieImage(response.results[0])
+
+        })
+
+    }, [])
+
+    return (
+        <div style={{ width: '100%', margin: '0' }}>
+
+            {/* Main Image */}
+            {MainMovieImage &&
+                <MainImage
+                image={`${IMAGE_BASE_URL}w1200${MainMovieImage.backdrop_path}`}
+                title={MainMovieImage.original_title}
+                text={MainMovieImage.overview}
+                />
+            }
+            <div style={{width: '85', margin: '1rem auto'}}>
+
+                <h2>Movies by latest</h2>
+                <hr />
+
+                {/* Movie Grid Cards*/}
+
+            </div>
+            
+            <div style={{ display: 'flex', justifyContent: 'center'}}>
+                <button>Load More</button>
+            </div>
+        </div>
+    
     )
 }
 
