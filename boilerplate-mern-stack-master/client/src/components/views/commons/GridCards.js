a1335ceffccd3e52493bd14717a8fc5926cfed1c
unknown <lerle144@gmail.com>
2022-05-03 22:01:08 +0900

    7강 Movie Detail 페이지 만들기

--- boilerplate-mern-stack-master/client/src/components/views/commons/GridCards.js
+++ boilerplate-mern-stack-master/client/src/components/views/commons/GridCards.js
@@ -4,7 +4,7 @@ function GridCards(props) {
   return (
     <Col lg={6} md={8} xs={24}>
       <div style={{ porition: 'relative'}} >
-          <a href={`/movie/${props.movieId}</div>`} >
+          <a href={`/movie/${props.movieId}`} >
               <img style={{ width: '100%', height: '320px' }} src={props.image} alt={props.movieName}/>
           </a>
       </div>
