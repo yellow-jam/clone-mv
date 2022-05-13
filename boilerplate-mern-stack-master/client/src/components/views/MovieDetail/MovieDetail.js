cd1bb5b7c27d87ac58c052f57f66ca1e147bc06b
unknown <lerle144@gmail.com>
2022-05-13 22:29:29 +0900

    8강 출연진들 가져오기 토글버튼으로 구현

--- boilerplate-mern-stack-master/client/src/components/views/MovieDetail/MovieDetail.js
+++ boilerplate-mern-stack-master/client/src/components/views/MovieDetail/MovieDetail.js
@@ -10,7 +10,8 @@ function MovieDetail(props) {
     let movieId = props.match.params.movieId
     const [Movie, setMovie] = useState([])
     const [Casts, setCasts] = useState([])
-    
+    const [ActorToggle, setActorToggle] = useState(false)
+
     useEffect(() => {
 
         let endpointCrew = `${API_URL}movie/${movieId}/credits?api_key=${API_KEY}`
@@ -33,6 +34,10 @@ function MovieDetail(props) {
             
 
     }, [])
+
+    const toggleActorView = () => {
+        setActorToggle(!ActorToggle);
+    }
 
 
   return (
@@ -64,23 +69,26 @@ function MovieDetail(props) {
             {/* Actors Grid */}
 
             <div style={{ display: 'flex', justifyContent: 'center', margin: '2rem' }}>
-                <button > Toggle Actor View</button>
+                <button onClick={toggleActorView}> Toggle Actor View</button>
             </div>
                 
 
-            <Row gutter={[16, 16]}>
+            {ActorToggle && 
+               <Row gutter={[16, 16]}>
 
-                {Casts && Casts.map((cast, index) => (
-                    <React.Fragment key={index}>
-                        <GridCards
-                            image={cast.profile_path ?
-                            `${IMAGE_BASE_URL}w500${cast.profile_path}`: null}
-                            characterName={cast.name}
-                        />
-                    </React.Fragment>
-                ))}
+                    {Casts && Casts.map((cast, index) => (
+                        <React.Fragment key={index}>
+                            <GridCards
+                                image={cast.profile_path ?
+                                `${IMAGE_BASE_URL}w500${cast.profile_path}`: null}
+                                characterName={cast.name}
+                            />
+                        </React.Fragment>
+                    ))}
 
-            </Row>
+                </Row>
+            }
+            
 
         </div>
 
