b4863c67105aa345a3326c8532063d5830798710
unknown <lerle144@gmail.com>
2022-05-15 07:49:37 +0900

    13강 2. MongoDB에서 Favorite로 된 영화 정보들을 가져오기 - useEffect, route

--- boilerplate-mern-stack-master/client/src/components/views/FavoritePage/FavoritePage.js
+++ boilerplate-mern-stack-master/client/src/components/views/FavoritePage/FavoritePage.js
@@ -1,7 +1,20 @@
-import React from 'react'
+import Axios from 'axios';
+import React, { useEffect } from 'react'
 import './favorite.css';
 
 function FavoritePage() {
+    useEffect(() => {
+
+        Axios.post('/api/favorite/getFavoredMovie', { userFrom: localStorage.getItem('userId') })
+            .then(response => {
+                if(response.data.success) {
+                    console.log(response.data)
+                } else {
+                    alert('영화 정보를 가져오는 데 실패했습니다.')
+                }
+            })
+    })
+
   return (
     <div style={{ width: '85%', margin: '3rem auto' }}>
       <h2> Favorite Movies </h2>
--- boilerplate-mern-stack-master/server/routes/favorite.js
+++ boilerplate-mern-stack-master/server/routes/favorite.js
@@ -66,10 +66,17 @@ router.post('/addToFavorite', (req, res) => {
         return res.status(200).json({ success: true })
     })
 
-
-
 })
 
+/* 13강 좋아요 페이지 */
+router.post('/getFavoredMovie', (req, res) => { 
 
+    Favorite.find({ 'userFrom': req.body.userFrom })
+        .exec((err, favorites) => {
+            if(err) return res.status(400).send(err)
+            return res.status(200).json({ success: true, favorites })
+        })
+
+})
 
 module.exports = router;
